2df0668451bd3a9d5a9f43f949fc8941b33d272c
spencerkoller <spencerkoller@outlook.com>
2019-01-29 12:06:42 -0700

    Creating practice.sh file & storing regec_practice.sh

---
+++ practice.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# Author : Spencer Koller
+# Date 1/31/2019
+# Script follows here:
+echo "Enter a number: "
+read numOne
+echo "Enter a second number: "
+read numTwo
+sum=$(($numOne + $numTwo))
+echo "The sum is : $sum"
+let prod=numOne*numTwo
+echo "The product is: $prod"
+grep $1 $2
